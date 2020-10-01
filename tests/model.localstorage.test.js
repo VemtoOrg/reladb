d24875335a7b7718ee1d8eabbf7f22ffc57a3f37
Tiago Rodrigues <tiagosilvapereira3@gmail.com>
2020-10-01 19:15:20 -0300

    wip

--- tests/model.localstorage.test.js
+++ tests/model.localstorage.test.js
@@ -3,3 +3,5 @@ const { default: LocalStorage } = require('../src/Drivers/LocalStorage');
 window.RelaDBDriver = LocalStorage
 
 require('./model-tests')
+
+afterAll(() => window.RelaDBDriver.clear())
--- tests/relationship.localstorage.test.js
+++ tests/relationship.localstorage.test.js
@@ -3,3 +3,5 @@ const { default: LocalStorage } = require('../src/Drivers/LocalStorage');
 window.RelaDBDriver = LocalStorage
 
 require('./relationship-tests')
+
+afterAll(() => window.RelaDBDriver.clear())
