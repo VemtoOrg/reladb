342a5cb4be46d9927379d38687be893146bced6f
Tiago Rodrigues <tiagosilvapereira3@gmail.com>
2020-10-02 18:27:04 -0300

    Adding eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
