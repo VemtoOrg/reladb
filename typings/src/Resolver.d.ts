0170ebfcab323b8509d2ca890b68040dd7175d4a
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2023-05-19 15:15:08 -0300

    Fixing typings for resolver

--- typings/src/Resolver.d.ts
+++ typings/src/Resolver.d.ts
@@ -2,8 +2,10 @@ export = Resolver;
 declare class Resolver {
     /** @type Database */
     static database: Database;
+    static databaseReadyCallbacks: any[];
     static setDatabase(database: any): void;
     static db(): import("./Database");
+    static onDatabaseReady(callback: any): void;
 }
 declare namespace Resolver {
     export { Database };
